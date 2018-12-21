c59c7621fd14cf9eed9970dec3796e3689643ecf
Pawin Piemthai <pawin35@hotmail.com>
2018-12-21 10:45:39 +0700

    uploading code

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.d
@@ -0,0 +1,86 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/stm-workspace-test/Embeded-Project-2/Inc/stm32f4xx_hal_conf.h \
+ D:/stm-workspace-test/Embeded-Project-2/Inc/main.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cm4.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Inc/stm32f4xx_hal_conf.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Inc/main.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+D:/stm-workspace-test/Embeded-Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
